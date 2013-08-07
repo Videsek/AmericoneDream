1024c71239db79ae8d5fefeb6903d1735e40bac8
videsek <videsek@gmail.com>
2013-08-06 21:06:57 -0700

    initial set up

---
+++ AmericoneDream/AmericoneDream/AmericoneDream/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	//create a muthafucking window
+	sf::RenderWindow window(sf::VideoMode(1024,768),"fucking window");
+	sf::CircleShape shape(500.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while(window.isOpen())
+	{
+		sf::Event eventy;
+		while(window.pollEvent(eventy))
+		{
+			if(eventy.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
